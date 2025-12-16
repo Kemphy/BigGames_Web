3b1d22d0c05f8ce3d71e5a1b5f217123efda165d
nalen <dimaskempi79@@gmail.com>
2025-12-16 17:07:31 +0700

    Initial commit: BigGames rental PS website prototype

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
-import { BookingProvider } from "./context/BookingContext"
 import { AuthProvider } from "./context/AuthContext"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <BookingProvider>
-        <App />
-      </BookingProvider>
+      <App />
     </AuthProvider>
   </React.StrictMode>
 )
